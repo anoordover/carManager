85ffec3d583b3358a229c3a65150c7e1f5e0960a
anoordover <anoordover@gmail.com>
2015-11-30 09:06:13 +0100

    update dependencies

--- src/main/java/nl/hetcak/cronacle/view/CarActionsController.java
+++ src/main/java/nl/hetcak/cronacle/view/CarActionsController.java
@@ -1,17 +1,12 @@
 package nl.hetcak.cronacle.view;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.CheckBoxTreeItem;
 import javafx.scene.control.TextArea;
-import javafx.scene.control.TreeItem;
-import javafx.scene.control.TreeView;
-import nl.hetcak.cronacle.MainApp;
 import nl.hetcak.cronacle.extraction.DefinitionExtractor;
 import nl.hetcak.cronacle.extraction.Extractor;
 import nl.hetcak.cronacle.extraction.MetaInfExtractor;
 import nl.hetcak.cronacle.extraction.SupportingClassesExtractor;
 import nl.hetcak.cronacle.logging.TextAreaAppender;
-import nl.hetcak.cronacle.model.ShortNameFile;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -25,7 +20,6 @@ import org.apache.logging.log4j.core.layout.PatternLayout;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
@@ -54,7 +48,7 @@ public class CarActionsController {
     private void initialize() {
         LoggerContext ctx = (LoggerContext) LogManager.getContext(false);
         final Configuration config = ctx.getConfiguration();
-        Layout layout = PatternLayout.createLayout(PatternLayout.DEFAULT_CONVERSION_PATTERN, config, null, null, true, true, null, null);
+        Layout layout = PatternLayout.createLayout("%m%n", null, config, null, null, true, true, null, null);
         Appender appender = TextAreaAppender.createAppender("textAreaAppender", null, layout, textArea);
         appender.start();
         config.addAppender(appender);
